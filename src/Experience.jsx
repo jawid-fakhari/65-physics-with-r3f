d56565625999b7754393f586ab30829a6e61005e
jawid-fakhari <jawid_fakhari@live.it>
2024-10-17 16:08:27 +0200

    install repier and import physics

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,29 +1,29 @@
 import { OrbitControls } from '@react-three/drei'
+import { Physics } from '@react-three/rapier'
 import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
+        <mesh castShadow position={[- 2, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
+        <mesh castShadow position={[2, 2, 0]}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
+        <mesh receiveShadow position-y={- 1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
 
