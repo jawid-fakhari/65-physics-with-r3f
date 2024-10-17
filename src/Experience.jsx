07b6d10b35e6c970b8a2cc1ce801bb08355732d7
jawid-fakhari <jawid_fakhari@live.it>
2024-10-17 16:17:45 +0200

    rigidBody type

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -25,7 +25,7 @@ export default function Experience() {
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
 
-            <RigidBody>
+            <RigidBody type='fixed'>
                 <mesh receiveShadow position-y={- 1.25}>
                     <boxGeometry args={[10, 0.5, 10]} />
                     <meshStandardMaterial color="greenyellow" />
