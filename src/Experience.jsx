d20a54887440cc7a104055563f8d2cd4f028ef4f
jawid-fakhari <jawid_fakhari@live.it>
2024-10-17 16:11:44 +0200

    import RigidBody

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,5 +1,5 @@
 import { OrbitControls } from '@react-three/drei'
-import { Physics } from '@react-three/rapier'
+import { Physics, RigidBody } from '@react-three/rapier'
 import { Perf } from 'r3f-perf'
 
 export default function Experience() {
@@ -12,20 +12,26 @@ export default function Experience() {
         <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
         <ambientLight intensity={1.5} />
 
-        <mesh castShadow position={[- 2, 2, 0]}>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <Physics>
+            <RigidBody>
+                <mesh castShadow position={[- 2, 2, 0]}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+            </RigidBody>
 
-        <mesh castShadow position={[2, 2, 0]}>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <mesh castShadow position={[2, 2, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
 
-        <mesh receiveShadow position-y={- 1.25}>
-            <boxGeometry args={[10, 0.5, 10]} />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <RigidBody>
+                <mesh receiveShadow position-y={- 1.25}>
+                    <boxGeometry args={[10, 0.5, 10]} />
+                    <meshStandardMaterial color="greenyellow" />
+                </mesh>
+            </RigidBody>
 
+        </Physics>
     </>
 }
